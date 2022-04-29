961803e93d11e937808a9e198507f8966a7747e6
kenny caldieraro <kenny.caldieraro@gmail.com>
2022-04-29 19:57:10 +0200

    update server

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+
 const app = express();
-const router = require("./app/router");
+const router = require('./app/router');
+
 const PORT = process.env.PORT || 3000;
 
-app.set("view engine", "ejs");
-app.set("views", "./app/views");
-app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: '*',
+  }),
+);
+
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+app.use(express.static('public'));
+
 app.use(router);
 
 app.listen(PORT, () => {
