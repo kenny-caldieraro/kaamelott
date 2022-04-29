50ff06a9bc6c30128ede00350c330a7b7424ffe3
kenny caldieraro <kenny.caldieraro@gmail.com>
2022-04-29 20:09:04 +0200

    update router / server

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const express = require('express');
 
 const app = express();
+const multer = require('multer');
 const router = require('./app/router');
 
 const PORT = process.env.PORT || 3000;
@@ -15,12 +16,14 @@ app.use(
   }),
 );
 
+const bodyParser = multer();
+app.use(bodyParser.none());
+
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 app.use(express.static('public'));
 
 app.use(router);
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
